fcb7481949f597701cd4dfb1104a50271292d23e
Wilfred <wilfred5777@gmail.com>
2022-03-16 18:40:01 +0800

    https://www.youtube.com/watch?v=lupm5EBVvaE&list=PLKhlp2qtUcSYC7EffnHzD-Ws2xG-j3aYo&index=7

--- frontend/src/screens/LandingPage/LandingPage.js
+++ frontend/src/screens/LandingPage/LandingPage.js
@@ -1,9 +1,34 @@
 import React from "react";
+import { Button, Container, Row } from "react-bootstrap";
+import "./LandingStyles.css";
 
 function LandingPage() {
   return (
-    <div>
-      <div>Landing Page</div>
+    <div className="main">
+      <Container>
+        <Row>
+          <div className="intro-text">
+            <h1 className="title">Welcome to Note Zipper</h1>
+            <p className="subtitle">One safe Place for all your notes.</p>
+            <div className="buttonContainer">
+              <a href="/login">
+                <Button
+                  size="lg"
+                  className="landingbutton"
+                  variant="outline-dark"
+                >
+                  Login
+                </Button>
+              </a>
+              <a href="/register">
+                <Button size="lg" className="landingbutton" variant="dark">
+                  Sign Up
+                </Button>
+              </a>
+            </div>
+          </div>
+        </Row>
+      </Container>
     </div>
   );
 }
