daa36ddd3878a54d239f90ebed3bf76704da0d0b
Wilfred <wilfred5777@gmail.com>
2022-03-16 15:49:02 +0800

    essentials

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,25 @@
 const express = require("express");
-
 const app = express();
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
 
-app.listen(5000, console.log("Server started on Port 5000"));
+// https://www.youtube.com/watch?v=H1BebGmRjzE&list=PLKhlp2qtUcSYC7EffnHzD-Ws2xG-j3aYo&index=4
+
+// https://github.com/piyush-eon/notezipper
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const note = notes.find((n) => n._id === req.params.id);
+  //   console.log(req.params);
+  res.send(note);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on Port ${PORT}`));
