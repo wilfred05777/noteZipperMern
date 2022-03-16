06e5bb48b8b756178beb4d131891861beaa83c60
Wilfred <wilfred5777@gmail.com>
2022-03-16 19:39:48 +0800

    Create Note button

--- frontend/src/screens/MainScreen.js
+++ frontend/src/screens/MainScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
-import "./Screen.css";
+import "./MainScreen.css";
 
 function MainScreen({ children, title }) {
   return (
--- frontend/src/screens/MyNotes/MyNotes.js
+++ frontend/src/screens/MyNotes/MyNotes.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import MainScreen from "../MainScreen";
 
 function MyNotes() {
   return (
-    <div>
-      <>My NOtes</>
-    </div>
+    <MainScreen title="Welcome Back Wilfred">
+      <Link to="createnote">
+        <Button
+          variant="outline-dark"
+          style={{ marginLeft: 10, marginBottom: 6 }}
+          size="lg"
+        >
+          Create Note
+        </Button>
+      </Link>
+    </MainScreen>
   );
 }
 
